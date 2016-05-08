bebe6b3688cea251920897c4bf557255c5c38242
Simon1 <Simon1@Simon-PC>
2016-05-08 22:00:48 +0200

    BTree kleine Änderungen

--- uebung2/src/de/hs_mannheim/ss16/ib/tpe/g1_1/uebung2/teil2/aufgabe1/BTree.java
+++ uebung2/src/de/hs_mannheim/ss16/ib/tpe/g1_1/uebung2/teil2/aufgabe1/BTree.java
@@ -187,7 +187,7 @@ public class BTree implements BTreeInterface {
                 else
                     this.min = node.getElements()[i + 1];
             }
-            
+            BTreeNode parent = getParentOf(node);
             node.getElements()[i] = null;
             int indexOfDeletedElement = i;
             
@@ -230,12 +230,15 @@ public class BTree implements BTreeInterface {
                         } else {
                             replacement = getSmallestFromRightSubtree(node, indexOfDeletedElement, false);
                             if (replacement != null) {
-                                node.addVal(indexOfDeletedElement, replacement);
+                                node.addValInDelete(indexOfDeletedElement, replacement);
                                 // node.getElements()[indexOfDeletedElement] = getSmallestFromRightSubtree(node, indexOfDeletedElement);
                             } else {
                                 node.addValInDelete(indexOfDeletedElement, getGreatestFromLeftSubtree(node, indexOfDeletedElement, true));
 
                                 // the leaf with replacement has a underflow now -> you have to correct it
+                                BTreeNode newLeaf = new BTreeNode(1);
+                                BTreeNode[] references = node.getReferences();
+                                
                                 node = node.getReferences()[indexOfDeletedElement];
                                 while(node.hasReferences())
                                     node = node.getReferences()[node.getNumberOfElements()];
@@ -252,7 +255,6 @@ public class BTree implements BTreeInterface {
                             node.getElements()[j+1] = null;
                         }
                         if (checkUnderflow(node)) {
-                            BTreeNode parent = getParentOf(node);
                             BTreeNode leftSibling = null;
                             BTreeNode rightSibling = null;
                             int childIndex = 0;
--- uebung2/src/de/hs_mannheim/ss16/ib/tpe/g1_1/uebung2/teil2/aufgabe1/TestBTree.java
+++ uebung2/src/de/hs_mannheim/ss16/ib/tpe/g1_1/uebung2/teil2/aufgabe1/TestBTree.java
@@ -57,8 +57,10 @@ public class TestBTree {
 		for(int i = 0; i < a.length; i++ ){
 			test.insert(a[i]);
 		}
-		test.delete(18);
-		assertEquals(false,test.contains(10));
+		test.delete(1);
+		assertEquals(false,test.contains(1));
+		test.delete(2);
+		assertEquals(false,test.contains(2));
 		
 	}
 	
