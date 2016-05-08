2789ae4d8cff77ef1e93fb4da79216838aac08ee
Simon1 <Simon1@Simon-PC>
2016-05-08 23:13:43 +0200

    BTreeWithCarObjects jetzt unnötig.Testcar enthält jetzt alles

--- uebung2/src/de/hs_mannheim/ss16/ib/tpe/g1_1/uebung2/teil2/aufgabe3/TestCar.java
+++ uebung2/src/de/hs_mannheim/ss16/ib/tpe/g1_1/uebung2/teil2/aufgabe3/TestCar.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import de.hs_mannheim.ss16.ib.tpe.g1_1.uebung2.teil2.aufgabe1.BTree;
+
 public class TestCar {
 
 	@Test
@@ -23,6 +25,33 @@ public class TestCar {
 		HybridCar car3 = new HybridCar("Tesla",2012,25000,3,"Low_Voltage");
 		assertEquals(car3.getVoltage(),480);
 		assertEquals(car3.getEmissionTier(),3);
+	}
+	@Test
+	public void testCompareTo(){
+		//ID is compared(is incremented for each instance of Car).
+		Car bmw = new GasolineCar("BMW",2012,25000,2);
+		Car vw = new HybridCar("VW",2014,30000,1,"LOW_VOLTAGE");
+		assertEquals(0,bmw.compareTo(bmw));
+		assertEquals(-1,bmw.compareTo(vw));
+		assertEquals(1,vw.compareTo(bmw));
+	}
+	@Test
+	public void insertCarsIntoBTree(){
+		BTree cars = new BTree(2);
+		Car bmw = new GasolineCar("BMW",2012,25000,2);
+		Car audi = new GasolineCar("Audi",2011,22000,3);
+		Car tesla = new ElectricCar("Tesla",2015,50000,"HIGH_Voltage");
+		Car vw = new HybridCar("VW",2014,30000,1,"LOW_VOLTAGE");
+		cars.insert(bmw);
+		cars.insert(audi);
+		cars.insert(tesla);
+		cars.insert(vw);
+		assertEquals(true,cars.contains(bmw));
+		assertEquals(vw,cars.getMax());
+		assertEquals(bmw,cars.getMin());
+		cars.delete(vw);
+		assertEquals(false,cars.contains(vw));
+		
 	}
 
 }
